4fb037ec4f3b623bf6ae6eb239e679c10c63205b
Emanuel181 <92999481+Emanuel181@users.noreply.github.com>
2021-10-29 22:25:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Introduceti expresia ce va fi verificata: ", end = '')
+print("Introduceti expresia ce va fi verificata: ", end='')
 
-prop, cnt_c, cnt_p, poz, conectori, ok = input(), 0, 0, 0, ['∧', '∨', '⇒', '¬', '⇔'], True
+prop, cnt_c, cnt_p, poz, conectori = input(), 0, 0, 0, ['∧', '∨', '⇒', '¬', '⇔']
 
 def RemoveSpaces(exp):
     return "".join(exp.split())
@@ -9,13 +9,12 @@ prop = RemoveSpaces(prop)
 
 print()
 
-
 for i in prop:
     if (i in conectori or i == ')') and poz == 0:
         print("Conectorii sau paranteza inchisa nu poate sa apara pe prima pozitie. Nu, expresia nu este o propozitie bine definita.")
         exit()
 
-    if i in conectori and (i != '¬' and poz < 2):
+    if i in conectori and (i != '¬' and poz <= 1 ):
         print("Un conector binar apare cel mai devreme pe pozitia 3. Nu, expresia nu este o propozitie bine definita.")
         exit()
 
@@ -31,66 +30,68 @@ for i in prop:
     poz += 1
 
 
-if 2*cnt_c - cnt_p == 0:
-    for i in range(len(prop)):
-        if prop[i] == '(':
-            print("POZITIA[", i, ']:', prop[i])
-            print("Propozitia vrea sa fie una complexa, ma astept la o paranteza, o negatie sau o propozitie atomica.")
-            if prop[i + 1] == '(' or (prop[i + 1] == '¬' or 'A' <= prop[i + 1] <= 'Z'):
-                continue
-            else:
-                ok = False
-                print("Pe pozitia", i, "se afla o paranteza, iar pe pozitia urmatoare se astepta o negatie, o paranteza deschisa sau o propozitie atomica, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
-                break
+for i in range(len(prop)):
+    if prop[i] == '(':
+        print("POZITIA[", i, ']:', prop[i])
+        print("Propozitia vrea sa fie una complexa, ma astept la o paranteza, o negatie sau o propozitie atomica.")
+        if prop[i + 1] == '(' or (prop[i + 1] == '¬' or 'A' <= prop[i + 1] <= 'Z'):
+            continue
+        else:
+            print("Pe pozitia", i, "se afla o paranteza, iar pe pozitia urmatoare se astepta o negatie, o paranteza deschisa sau o propozitie atomica, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
+            exit()
 
-        if prop[i] == '¬':
-            print("POZITIA[", i, ']:', prop[i])
-            print("Am un conector binar, ma astept la o propozitie atomica sau la o paranteza deschisa.")
-            if 'A' <= prop[i + 1] <= 'Z' or prop[i + 1] == '(':
-                continue
-            else:
-                ok = False
-                print("Pe pozitia", i, "se afla o negatie iar pe pozitia urmatoare se asteapta o propozitie atomica sau o paranteza deschisa, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
-                break
+    if prop[i] == '¬':
+        print("POZITIA[", i, ']:', prop[i])
+        print("Am un conector binar, ma astept la o propozitie atomica sau la o paranteza deschisa.")
+        if 'A' <= prop[i + 1] <= 'Z' or prop[i + 1] == '(':
+            continue
+        else:
+            print("Pe pozitia", i, "se afla o negatie iar pe pozitia urmatoare se asteapta o propozitie atomica sau o paranteza deschisa, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
+            exit()
 
-        if 'A' <= prop[i] <= 'Z' and (prop[i - 1] != '∧' and prop[i - 1] != '∨' and prop[i - 1] != '⇒' and prop[i - 1] != '¬' and prop[i - 1] != '⇔'):
-            print("POZITIA[", i, ']:', prop[i])
-            print("Am o propozitie atomica care nu e precedata de un conector, ma astept la un conector.")
-            if prop[i + 1] in conectori:
-                continue
-            else:
-                ok = False
-                print("Pe pozitia", i, "se afla o propozitie atomica, iar pe urmatoarea pozitie se asteapta obligatoriu un conector, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
-                break
+    if 'A' <= prop[i] <= 'Z' and prop[i - 1] not in conectori:
+        print("POZITIA[", i, ']:', prop[i])
+        print("Am o propozitie atomica care nu e precedata de un conector, ma astept la un conector.")
+        if prop[i + 1] in conectori:
+            continue
+        else:
+            print("Pe pozitia", i, "se afla o propozitie atomica, iar pe urmatoarea pozitie se asteapta obligatoriu un conector, dar nu e prezent. Nu, expresia nu este o propozitie bine definita.")
+            exit()
 
-        if 'A' <= prop[i] <= 'Z' and prop[i - 1] is conectori:
-            print("POZITIA[", i, ']:', prop[i])
-            print("Am o propozitie atomica care e precedata de un conector, ma astept la o paranteza inchisa.")
-            if prop[i + 1] == ')':
-                continue
-            else:
-                ok = False
-                print("Pe pozitia", i, "se afla o propozitie atomica ce este precedata de un conector, iar pe urmatoarea pozitie se asteapta obligatoriu o paranteza, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
-                break
+    if 'A' <= prop[i] <= 'Z' and prop[i - 1] in conectori:
+        print("POZITIA[", i, ']:', prop[i])
+        print("Am o propozitie atomica care e precedata de un conector, ma astept la o paranteza inchisa.")
+        if prop[i + 1] == ')':
+            continue
+        else:
+            print("Pe pozitia", i, "se afla o propozitie atomica ce este precedata de un conector, iar pe urmatoarea pozitie se asteapta obligatoriu o paranteza, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
+            exit()
 
-        if prop[i] in conectori:
-            print("POZITIA[", i, ']:', prop[i])
-            print("Am un conector, ma astept la o paranteza deschisa sau la o propozitie atomica.")
-            if prop[i + 1] == '(' or 'A' <= prop[i + 1] <= 'Z':
-                continue
-            else:
-                ok = False
-                print("Pe pozitia", i, "se afla un conector, iar pe urmatoarea pozitie se asteapta fie o paranteza deschisa, fie o propozitie atomica, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
-                break
+    if prop[i] in conectori:
+        print("POZITIA[", i, ']:', prop[i])
+        print("Am un conector, ma astept la o paranteza deschisa sau la o propozitie atomica.")
+        if prop[i + 1] == '(' or 'A' <= prop[i + 1] <= 'Z':
+            continue
+        else:
+            print("Pe pozitia", i, "se afla un conector, iar pe urmatoarea pozitie se asteapta fie o paranteza deschisa, fie o propozitie atomica, dar nu e prezenta niciuna. Nu, expresia nu este o propozitie bine definita.")
+            exit()
 
-else:
-    if (cnt_p - 2 * cnt_c) % 2 == 0 and cnt_p - 2*cnt_c > 0 and cnt_c > 0:
+    if prop[i] == ')':
+        print("POZITIA[", i, ']:', prop[i])
+        print("Pe pozitia", i, "se afla o paranteza inchisa, deci acum s-a inchis o expresie complexa")
+        continue
+
+if cnt_p - 2*cnt_c>= 2:
+    if (cnt_p - 2 * cnt_c) % 2 == 0 and cnt_c > 0:
+        print()
         print("Se pot elimina", cnt_p - 2*cnt_c, "paranteze din captele expresiei si aceasta isi pastreaza sensul. Da, expresia este o propozitie bine definita.")
         exit()
     else:
+        print()
         print("Numarul de paranteze nu corespunde cu numarul de conectori. Nu, expresia nu este o propozitie bine definita.")
         print("Sunt", cnt_c, "conectori si", cnt_p, "paranteze.")
         exit()
-
-if ok:
-    print("Da, expresia este o propozitie bine definita.")
+else:
+    if i == len(prop) - 1:
+        print()
+        print("Da, expresia este o propozitie bine definita.")
